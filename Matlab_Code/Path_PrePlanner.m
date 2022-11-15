58be3980cc3511b17bd731ccf5325e87452631e4
Wyatt <wyattkromer@gmail.com>
2022-11-15 18:57:20 -0500

    Bug Fixes in Matlab

--- Matlab_Code/Path_PrePlanner.m
+++ Matlab_Code/Path_PrePlanner.m
@@ -19,8 +19,8 @@ clc
 
 max_v = 2;              % m/s
 max_a = .05;              % m/s
-time_to_max_a = .5;      % s
-PLC_dt = 0.01;          % s
+time_to_max_a = 20;      % s
+PLC_dt = 1;          % s
 goal = 1000;               % m
 deadband_distance = 0; % m, and this is for a single direction. Deadband is actually twice as wide
 
@@ -64,41 +64,41 @@ time_total = t7;
 % Calculate Profile
 %%%%%%%%%%%%%%%%%%%
 dt = PLC_dt / 100; %So we can plot a much more specific curve to see whatprofile actually looks like
-% t_rec = zeros(1,time_total/dt);
+t_rec = zeros(1,time_total/dt);
 % x_rec = zeros(1,time_total/dt);
 % v_rec = zeros(1,time_total/dt);
 % a_rec = zeros(1,time_total/dt);
 
 x_rec(1) = deadband_distance;
 t = 1;
-while t < time_total
+while t_rec(t) < time_total
     t = t + 1;
     t_rec(t) = t*dt;
-    if t < time_to_max_a 
+    if t_rec(t) < time_to_max_a 
         % Ramp up acceleration
         a_rec(t) = (j * t_rec(t));
         v_rec(t) = (0.5 * j * t_rec(t)^2);
-    elseif t < t2
+    elseif t_rec(t) < t2
         % Increase speed at max acceleration
         a_rec(t) = (max_a);
-        v_rec(t) = (max_a * (t - 0.5 * time_to_max_a ));
-    elseif t < t3
+        v_rec(t) = (max_a * (t_rec(t) - 0.5 * time_to_max_a));
+    elseif t_rec(t) < t3
         % Ramp down acceleration
         a_rec(t) = (max_a - j * (t_rec(t) - t2));
-        v_rec(t) = (max_a * (t - 0.5 * time_to_max_a ) - 0.5 * j * (t_rec(t) - t2) ^ 2);
-    elseif t < t4
+        v_rec(t) = (max_a * (t_rec(t) - 0.5 * time_to_max_a ) - 0.5 * j * (t_rec(t) - t2) ^ 2);
+    elseif t_rec(t) < t4
         % Maintain max velocity
-        a_rec(t) = (0.0);
-        v_rec(t) = (profile_max_v);
-    elseif t < t5
+        a_rec(t) = 0;
+        v_rec(t) = profile_max_v;
+    elseif t_rec(t) < t5
         % Ramp down acceleration
         a_rec(t) = (-j * (t_rec(t) - t4));
         v_rec(t) = ( profile_max_v - 0.5 * j * (t_rec(t) - t4) ^ 2);
-    elseif t < t6
+    elseif t_rec(t) < t6
         % Decrease speed at max acceleration
         a_rec(t) = (-max_a);
         v_rec(t) = (max_a * (t2 + t5 - t_rec(t) - 0.5 * time_to_max_a));
-    elseif t < t7
+    elseif t_rec(t) < t7
         % Ramp up acceleration
         a_rec(t) = (-max_a + j * (t_rec(t) - t6));
         v_rec(t) = (max_a * (t2 + t5 - t_rec(t) - 0.5 * time_to_max_a) + 0.5 * j * (t_rec(t) - t6) ^ 2);
@@ -117,8 +117,6 @@ end
 figure
 subplot(311)
 plot(t_rec,x_rec,'LineWidth',2)
-hold on
-xline(time_to_max_a)
 ylabel("Distance [m]")
 xlabel("Time [s]")
 subplot(312)
@@ -129,4 +127,3 @@ subplot(313)
 plot(t_rec,a_rec,'LineWidth',2)
 ylabel("Acceleration [m/s^2]")
 xlabel("Time [s]")
-xline(time_to_max_a)
